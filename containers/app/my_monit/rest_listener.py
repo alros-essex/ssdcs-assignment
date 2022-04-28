6e0516edf9fc1226cf7ead49ed2062b9537b4b56
ros101 <ar21868@essex.ac.uk>
2022-04-28 07:24:59 +0200

    refactoring

--- containers/app/my_monit/rest_listener.py
+++ containers/app/my_monit/rest_listener.py
@@ -1,5 +1,6 @@
 '''REST APIs'''
 
+from abc import ABC
 from flask import Flask, request, jsonify
 import jwt
 
@@ -20,6 +21,111 @@ class RestConfiguration():
         '''returns hostname'''
         return self._rest_host
 
+class FlaskResource(ABC):
+
+    def __init__(self, logging:Logging) -> None:
+        self._logging = logging
+
+    def log_info(self, msg:str, metadata):
+        self._logging.info(msg = msg, metadata = metadata)
+
+    def get_user(self):
+        token = request.headers['Authorization'].split()[1]
+        decoded = jwt.decode(token, "secret", algorithms=["HS256"])
+        username = decoded['username']
+        return username
+
+    def metadata(self, api:str, method:str, user:str):
+        return {
+            'service': 'RestListener',
+            'api': api,
+            'method': method,
+            'user': user
+        }
+
+class MeasuresResource(FlaskResource):
+
+    def __init__(self, measures_service:MeasuresService, logging: Logging) -> None:
+        super().__init__(logging)
+        self._measure_service = measures_service
+
+    def get(self, experiment_id:int):
+        '''retrieve measures for an experiment'''
+        user_id = self.get_user()
+        self.log_info(f'called API', metadata = self.metadata(api = '/measures/<int:experiment_id>',
+                                                              method = 'GET',
+                                                              user = user_id))
+        #TODO
+        page = 1
+        measures = self._measure_service.retrieve_measures(experiment_id = experiment_id,
+                                                            current_user = user_id,
+                                                            page = page)
+        return jsonify(measures), 200
+
+class ExperimentResource(FlaskResource):
+
+    def __init__(self, experiment_service:ExperimentService, logging: Logging) -> None:
+        super().__init__(logging)
+        self._experiment_service = experiment_service
+
+    def get(self):
+        '''retrieves all experiments'''
+        user_id = self.get_user()
+        self._logging.info(f'called API', metadata = self.metadata(api = '/experiments/',
+                                                                   method = 'GET',
+                                                                   user = user_id))
+        experiments = self._experiment_service.retrieve_experiments(current_user = user_id)
+        return jsonify(experiments), 200
+
+    def post(self):
+        '''create a new experiment'''
+        user_id = self.get_user()
+        self._logging.info(f'called API', metadata = self.metadata(api = '/experiments/',
+                                                                   method = 'POST',
+                                                                   user = user_id))
+        self._experiment_service.insert_experiment(experiment_dict = request.json,
+                                                       current_user = user_id)
+        return 'created', 201
+
+class UserResouce(FlaskResource):
+
+    def __init__(self, user_service:UserService, logging: Logging) -> None:
+        super().__init__(logging)
+        self._user_service = user_service
+
+    def get(self):
+        '''retrieves all users'''
+        user_id = self.get_user()
+        self._logging.info(f'called API', metadata = self.metadata(api = '/users/',
+                                                                   method = 'GET',
+                                                                   user = user_id))
+        users = self._user_service.retrieve_users(user_id)
+        return jsonify(users), 200
+
+    def post(self):
+        '''creates a user'''
+        user_id = self.get_user()
+        self._logging.info(f'called API', metadata = self.metadata(api = '/users/',
+                                                                   method = 'POST',
+                                                                   user = user_id))
+        self._user_service.insert_user(current_user = user_id,
+                                       user_dict = request.json)
+        return 'created', 201
+
+class ExceptionResource(FlaskResource):
+
+    def __init__(self, logging: Logging) -> None:
+        super().__init__(logging)
+
+    def handle_error(self, exception, message:str, http_status:int):
+        log = self._logging.warn if http_status<500 else self._logging.error
+        log(f'exception in rest call', metadata = {
+            'service': 'RestListener',
+            'exception': str(exception),
+            'httpstatus': http_status
+        })
+        return message, http_status
+
 class RestListener():
     '''Creates an HTTP listener'''
 
@@ -30,10 +136,12 @@ class RestListener():
                  user_service:UserService,
                  logging: Logging) -> None:
         self._configuration = configuration
-        self._experiment_service = experiment_service
-        self._measures_service = measures_service
-        self._user_service = user_service
-        self._logging = logging
+        self._measure_resource = MeasuresResource(measures_service = measures_service,
+                                                  logging = logging)
+        self._experiment_resource = ExperimentResource(experiment_service = experiment_service,
+                                                       logging = logging)
+        self._user_resource = UserResouce(user_service = user_service, logging = logging)
+        self._exception_resource = ExceptionResource(logging = logging)
 
     def run(self):
         '''Main method that runs the listener'''
@@ -62,79 +170,31 @@ class RestListener():
         @app.route("/measures/<int:experiment_id>", methods=['GET'])
         def get_measures(experiment_id:int):
             '''retrieve measures for an experiment'''
-            user_id = get_user()
-            self._logging.info(f'called API', metadata = {
-                'service': 'RestListener',
-                'api': '/measures/<int:experiment_id>',
-                'method': 'GET',
-                'user': user_id,
-                'experiment_id': experiment_id
-            })
-
-            #TODO
-            page = 1
-            measures = self._measures_service.retrieve_measures(experiment_id = experiment_id,
-                                                       current_user = user_id,
-                                                       page = page)
-            return jsonify(measures), 200
+            return self._measure_resource.get(experiment_id = experiment_id)
 
         # Experiments
 
         @app.route("/experiments/", methods=['GET'])
         def get_experiments():
             '''retrieves all experiments'''
-            user_id = get_user()
-            self._logging.info(f'called API', metadata = {
-                'service': 'RestListener',
-                'api': '/experiments/',
-                'method': 'GET',
-                'user': user_id
-            })
-            experiments = self._experiment_service.retrieve_experiments(current_user = user_id)
-            return jsonify(experiments), 200
+            return self._experiment_resource.get()
 
         @app.route("/experiments/", methods=['POST'])
         def post_experiment():
             '''create a new experiment'''
-            user_id = get_user()
-            self._logging.info(f'called API', metadata = {
-                'service': 'RestListener',
-                'api': '/experiments/',
-                'method': 'POST',
-                'user': user_id
-            })
-            self._experiment_service.insert_experiment(experiment_dict = request.json,
-                                                       current_user = user_id)
-            return 'created', 201
+            return self._experiment_resource.post()
 
         # Users
 
         @app.route("/users/", methods=['GET'])
         def get_users():
             '''retrieves all users'''
-            user_id = get_user()
-            self._logging.info(f'called API', metadata = {
-                'service': 'RestListener',
-                'api': '/users/',
-                'method': 'GET',
-                'user': user_id
-            })
-            users = self._user_service.retrieve_users(user_id)
-            return jsonify(users), 200
+            return self._user_resource.get()
 
         @app.route("/users/", methods=['POST'])
         def create_users():
             '''creates a user'''
-            user_id = get_user()
-            self._logging.info(f'called API', metadata = {
-                'service': 'RestListener',
-                'api': '/users/',
-                'method': 'POST',
-                'user': user_id
-            })
-            self._user_service.insert_user(current_user = user_id,
-                                                   user_dict = request.json)
-            return 'created', 201
+            return self._user_resource.post()
 
         # Utilities
 
@@ -145,48 +205,30 @@ class RestListener():
         @app.errorhandler(AuthorizationException)
         def handle_authorization_error(exception):
             '''user is not authorized'''
-            self._logging.warn(f'exception in rest call', metadata = {
-                'service': 'RestListener',
-                'exception': str(exception),
-                'httpstatus': 401
-            })
-            return handle(exception, 'forbidden', 401)
+            return self._exception_resource.handle_error(exception = exception,
+                                                         message = 'forbidden',
+                                                         http_status = 401)
 
         @app.errorhandler(DbIntegrityError)
         def handle_db_integrity_error(exception):
             '''some db constrain was violated'''
-            self._logging.warn(f'exception in rest call', metadata = {
-                'service': 'RestListener',
-                'exception': str(exception),
-                'httpstatus': 400
-            })
-            return handle(exception, 'bad request', 400)
+            return self._exception_resource.handle_error(exception = exception,
+                                                         message = 'bad request',
+                                                         http_status = 400)
         
         @app.errorhandler(InvalidArgument)
         def handle_invalid_argument(exception):
             '''input is invalid'''
-            self._logging.warn(f'exception in rest call', metadata = {
-                'service': 'RestListener',
-                'exception': str(exception),
-                'httpstatus': 400
-            })
-            return handle(exception, 'bad request', 400)
+            return self._exception_resource.handle_error(exception = exception,
+                                                         message = 'bad request',
+                                                         http_status = 400)
 
         @app.errorhandler(Exception)
         def handle_internal_error(exception):
             '''default error handler'''
-            self._logging.error(f'exception in rest call', metadata = {
-                'service': 'RestListener',
-                'exception': str(exception),
-                'httpstatus': 500
-            })
-            return handle(exception, 'internal error', 500)
-
-        def get_user():
-            token = request.headers['Authorization'].split()[1]
-            decoded = jwt.decode(token, "secret", algorithms=["HS256"])
-            username = decoded['username']
-            return username
+            return self._exception_resource.handle_error(exception = exception,
+                                                         message = 'internal error',
+                                                         http_status = 500)
 
         # App
 
