418c88578933942f848ccfbcc5901347c4f8c37c
ros101 <ar21868@essex.ac.uk>
2022-04-30 08:37:21 +0200

    add tests

--- containers/app/test/test_rest_listener.py
+++ containers/app/test/test_rest_listener.py
@@ -3,9 +3,18 @@
 import logging
 import unittest
 from unittest.mock import Mock, MagicMock
-from my_monit.rest_listener import MeasuresResource, ExperimentResource, UserResouce, ExceptionResource
+from my_monit.rest_listener import MeasuresResource
+from my_monit.rest_listener import ExperimentResource
+from my_monit.rest_listener import UserResouce
+from my_monit.rest_listener import ExceptionResource
+from my_monit.rest_listener import ExperimentAssociationResource
 
 from my_monit.model import Measure
+
+def mock_flask_utilities(service):
+    service.get_user = MagicMock(return_value = 'usr')
+    service.to_json = MagicMock(return_value = 'json')
+    service.get_request_json = MagicMock(return_value = 'json')
 
 class TestMeasuresResource(unittest.TestCase):
     '''tests for the rest service'''
@@ -15,8 +24,7 @@ class TestMeasuresResource(unittest.TestCase):
         self.log = Mock()
         self.measures = MeasuresResource(measures_service = self.measures_service,
                                          logging = self.log)
-        self.measures.get_user = MagicMock(return_value = 'usr')
-        self.measures.to_json = MagicMock(return_value = 'json')
+        mock_flask_utilities(self.measures)
 
     def test_get(self):
         _, ret = self.measures.get('exp', 1)
@@ -33,9 +41,7 @@ class TestExperimentResource(unittest.TestCase):
         self.log = Mock()
         self.experiments = ExperimentResource(experiment_service = self.experiment_service,
                                                   logging = self.log)
-        self.experiments.get_user = MagicMock(return_value = 'usr')
-        self.experiments.to_json = MagicMock(return_value = 'json')
-        self.experiments.get_request_json = MagicMock(return_value = 'json')
+        mock_flask_utilities(self.experiments)
 
     def test_get(self):
         _, ret = self.experiments.get()
@@ -66,9 +72,7 @@ class TestUserResource(unittest.TestCase):
         self.log = Mock()
         self.users = UserResouce(user_service = self.user_service,
                                  logging = self.log)
-        self.users.get_user = MagicMock(return_value = 'usr')
-        self.users.to_json = MagicMock(return_value = 'json')
-        self.users.get_request_json = MagicMock(return_value = 'json')
+        mock_flask_utilities(self.users)
 
     def test_get(self):
         _, ret = self.users.get()
@@ -104,6 +108,29 @@ class TestExceptionResource(unittest.TestCase):
         self.log.warn.assert_called()
         self.assertEqual(400, ret)
 
+class TestExperimentAssociationResource(unittest.TestCase):
+    '''tests for the experiment-user association'''
+
+    def setUp(self):
+        self.log = Mock()
+        self.experiment_service = Mock()
+        self.associations = ExperimentAssociationResource(experiment_service = self.experiment_service,
+                                                          logging = self.log)
+        mock_flask_utilities(self.associations)
+
+    def test_get(self):
+        _, ret = self.associations.get()
+        
+        self.experiment_service.get_associations.assert_called()
+        self.log.info.assert_called()
+        self.assertEqual(200, ret)
+
+    def test_post(self):
+        _, ret = self.associations.post('S1', 'Exp1')
+        
+        self.experiment_service.associate.assert_called()
+        self.log.info.assert_called()
+        self.assertEqual(200, ret)
 
 if __name__ == '__main__':
     unittest.main()
